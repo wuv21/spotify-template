bb06587d627a43477b4441b54b4e990ac69a78fa
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-10-28 14:23:26 -0700

    Entire project now works with webpack
    When you edit the CSS, HTML, or JS, the page will do a live reload.
    Finally have a good understanding of how webpack operates, and ideas
    for improving the current setup.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+// Loads up the html fragrment into memory
+var html = require('html!./app.html');
+// Automatically injects our own css
+require('style!css!./main.css');
+
+// Actually inserts the HTML into the DOM
+document.write(html);
+// We do this, that way we reload automatically on any HTML change
+
+var angular = require('angular');
+
 var data;
 var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
 var myApp = angular.module('myApp', [])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
+  console.log('controller loaded successfully');
   $scope.getSongs = function() {
     $http.get(baseUrl + $scope.track).success(function(response){
       data = $scope.tracks = response.tracks.items
-      
+      console.log('Hey we got this to work');      
     })
   }
   $scope.play = function(song) {
@@ -24,8 +36,3 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     }
   }
 })
-
-// Add tool tips to anything with a title property
-$('body').tooltip({
-    selector: '[title]'
-});

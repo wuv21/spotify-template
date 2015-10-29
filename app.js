f420ebf95ff4cc8d916a018ed0719d2130b869d0
Caleb Thorsteinson <caleb@thorsteinson.me>
2015-10-28 22:51:54 -0700

    Basic responses return from Spotify
    Fixed the form so that it doesn't constantly refresh. Now we're able
    to bind responses back into the main view. Just need to start doing
    interesting things with them.

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
+var baseUrl = 'https://api.spotify.com/v1/search'
 var myApp = angular.module('myApp', [])
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   $scope.audioObject = {}
+
   $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
-    })
+    // This makes a request to the spotify API, and then searches for the
+    // input track
+    $http.get(baseUrl, {
+      params: {
+        'q': $scope.track,
+        'type': 'track'
+      }
+    }).then(succ, fail);
+
+    function succ(response) {
+      // THEN if everything is successful, we add the results to our view
+      console.log('successful response!');
+      console.log(response);
+      data = $scope.tracks = response.data.tracks.items;
+    }
+    function fail() {
+      // But if it fails, we print a little error message to the console.
+      console.error('uh oh, our request failed! Try again?');
+    }
   }
+
   $scope.play = function(song) {
     if($scope.currentSong == song) {
       $scope.audioObject.pause()
@@ -23,4 +40,4 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
       $scope.currentSong = song
     }
   }
-})
+});
